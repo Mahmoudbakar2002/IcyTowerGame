2518e534d52888301c39063c155f6cfa606e9c8d
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-11 20:10:42 +0200

    set some default data in engine

--- src/com/bakar/assest/opengl/texture/TextureEngine.java
+++
@@ -1,8 +0,0 @@
-package com.bakar.assest.opengl.texture;
-
-import java.util.List;
-
-public class TextureEngine {
-    private List<Image> lst;
-
-}
--- src/com/fsci/games/views/GameEngine.java
+++ src/com/fsci/games/views/GameEngine.java
@@ -2,17 +2,31 @@ package com.fsci.games.views;
 
 import com.bakar.assest.opengl.ListenerPanel;
 
+import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 
 public class GameEngine extends ListenerPanel {
 
+
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrtho(0, 600, 0, 600, -1.0, 1.0);
     }
 
     @Override
     public void display(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+
+        gl.glBegin(GL.GL_LINES);
+            gl.glVertex2i(0,20);
+            gl.glVertex2i(600,20);
+        gl.glEnd();
+
     }
 
     @Override
