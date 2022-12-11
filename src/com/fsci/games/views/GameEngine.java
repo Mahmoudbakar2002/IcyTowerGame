f84fca2933f209df032d73966a5a7186a6bd63e7
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-11 19:03:43 +0200

    create game engine

---
+++ src/com/fsci/games/views/GameEngine.java
@@ -0,0 +1,25 @@
+package com.fsci.games.views;
+
+import com.bakar.assest.opengl.ListenerPanel;
+
+import javax.media.opengl.GLAutoDrawable;
+
+public class GameEngine extends ListenerPanel {
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+    }
+}
--- src/com/fsci/games/views/GameFrame.java
+++ src/com/fsci/games/views/GameFrame.java
@@ -1,13 +1,38 @@
 package com.fsci.games.views;
 
+import com.sun.opengl.util.FPSAnimator;
+
+import javax.media.opengl.GLCanvas;
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
+
+    // game panel data
+    private FPSAnimator fps;
+    private GLCanvas gamePanel;
+    private GameEngine engine;
+
 
     public GameFrame(){
         initialize();
     }
     public  void initialize(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+        //center the JFrame on the screen
+        this.setLocationRelativeTo(null);
+
+
+        engine=new GameEngine();
+
+        GLCanvas gamePanel=new GLCanvas();
+        gamePanel.addKeyListener(engine);
+        gamePanel.addGLEventListener(engine);
+        this.add(gamePanel, BorderLayout.CENTER);
+        // animator
+        fps =new FPSAnimator(gamePanel,60);
+        fps.start();
 
     }
 }
