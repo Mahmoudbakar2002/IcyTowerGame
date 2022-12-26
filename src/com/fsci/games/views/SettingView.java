42614aee9c80b07d9d733d6a8c97cd18645cf2be
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-26 23:53:33 +0200

    add SettingView

---
+++ src/com/fsci/games/views/SettingView.java
@@ -0,0 +1,25 @@
+package com.fsci.games.views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class SettingView extends  AbstractGameView {
+    private JLabel background;
+    private JLabel paper;
+    private Image bgImage,paperImage;
+
+    private Runnable backToMenu;
+
+    public SettingView(Runnable backToMenu){
+        this.backToMenu=backToMenu;
+
+
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        backToMenu.run();
+    }
+}
