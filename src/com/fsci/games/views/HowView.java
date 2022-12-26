6ea84e134f06161a5e3af740a30b942f7899b008
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-26 23:49:20 +0200

    hv

--- src/com/fsci/games/views/HowView.java
+++ src/com/fsci/games/views/HowView.java
@@ -2,9 +2,7 @@ package com.fsci.games.views;
 
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class HowView extends AbstractGameView{
 
