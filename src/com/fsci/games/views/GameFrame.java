f56c1f8383d0e1deb1754cdabf6181af2ebe10fb
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-26 23:09:15 +0200

    update game frame structure

--- src/com/fsci/games/views/GameFrame.java
+++ src/com/fsci/games/views/GameFrame.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+    private int cardsLength=0;
 
     // game panel data
     private FPSAnimator fps;
@@ -14,27 +17,56 @@ public class GameFrame extends JFrame {
     private GameEngine engine;
 
 
+    private MainMenu mainMenu;
+
+    private void changeView(int idx){
+        if(idx>cardsLength||idx<0)return;
+        String name=String.valueOf(idx);
+        cardLayout.show(cardPanel,name);
+        for(Component comp : cardPanel.getComponents()){
+            if(comp.getName().equalsIgnoreCase(name))
+                comp.requestFocusInWindow();
+        }
+    }
+    private void addView(Component panel){
+        panel.setName(String.valueOf(cardsLength));
+        cardPanel.add(panel,String.valueOf(cardsLength++));
+    }
+
+
     public GameFrame(){
         initialize();
     }
     public  void initialize(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(600, 600);
+        setSize(700, 700);
         setResizable(false);
         //center the JFrame on the screen
         this.setLocationRelativeTo(null);
 
+        cardLayout=new CardLayout();
+        cardPanel=new JPanel(cardLayout);
+        this.add(cardPanel,BorderLayout.CENTER);
+
 
         engine=new GameEngine();
 
-        GLCanvas gamePanel=new GLCanvas();
+        gamePanel=new GLCanvas();
         gamePanel.addKeyListener(engine);
         gamePanel.addGLEventListener(engine);
         this.addKeyListener(engine);
-        this.add(gamePanel, BorderLayout.CENTER);
+
         // animator
         fps =new FPSAnimator(gamePanel,70);
         fps.start();
+
+        mainMenu=new MainMenu(this::changeView);
+        mainMenu.setSize(getWidth(),getHeight());
+
+        addView(mainMenu);
+        addView(gamePanel);
+
+        changeView(0);
 
     }
 }
